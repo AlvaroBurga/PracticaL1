f4f44c8fef9dbe461144917c8dd90f1fcfef2751
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-12 16:41:41 -0500

    ax2

---
+++ src/javaapplication2/Matematica.java
@@ -0,0 +1,21 @@
+package javaapplication2;
+
+public class Matematica {
+
+    public void primo(){
+
+    }
+
+    public void mcd(){
+
+    }
+
+    public void fibonacci(){
+
+    }
+
+    public void factorial(){
+
+    }
+
+}
