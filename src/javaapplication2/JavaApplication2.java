d068705528e5e51f48879cc8bbe20fabe20344f8
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-12 17:30:26 -0500

    gaaa

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -22,51 +22,69 @@ public class JavaApplication2 {
          Scanner sc= new Scanner(System.in);
        boolean end = false;
        Matematica matematica = new Matematica();
-       while(true)
-       {
-            String a=sc.nextLine();
-            System.out.println(a);
-            switch(a)
-            {
-                case "1":
-                {
-                    break;
-                }
-                case "2":
-                {
-                    break;
-                }
-                case "3":
-                {
-                    break;
+       System.out.println("--- Bienvenido ---");
+       System.out.println("seleccione la aplicación que desee utilizar:");
+       System.out.println("1) Operaciones con numeros");
+       System.out.println("2) Jackson (no implementado)");
+       System.out.println("Opcion : ");
+       int b = Integer.parseInt(sc.nextLine());
 
-                }
-                case "4":
-                {
-                    while (true){
-                        System.out.println("Ingrese un número: ");
-                        String  num = sc.nextLine();
+       if (b == 1){
+           while(true)
+           {
+               System.out.println("seleccione la operación que desee utilizar:");
+               System.out.println("1) Verificar si el número es primo");
+               System.out.println("2) MCD");
+               System.out.println("3) Fibonacci");
+               System.out.println("4) Factorial");
+               System.out.println("Opcion : ");
 
-                        try {
-                            int n = Integer.parseInt(num);
-                            if (n > 0){
-                                matematica.factorial(n);
-                            }else {
-                                System.out.println("Debe ingresar un número positivo");
-                            }
-                        }catch (Exception e){
-                            System.out.println("Ingrese un número válido (entero positivo)");
-                        }
-                        break;
-                    }
+               String a=sc.nextLine();
+               System.out.println(a);
+               switch(a)
+               {
+                   case "1":
+                   {
+                       break;
+                   }
+                   case "2":
+                   {
+                       break;
+                   }
+                   case "3":
+                   {
+                       break;
 
-                }
-                default:
-                {
-                    end=true;
-                }
-             }
-    }
+                   }
+                   case "4":
+                   {
+                       while (true){
+                           System.out.println("Ingrese un número: ");
+                           String  num = sc.nextLine();
+
+                           try {
+                               int n = Integer.parseInt(num);
+                               if (n > 0){
+                                   matematica.factorial(n);
+                               }else {
+                                   System.out.println("Debe ingresar un número positivo");
+                               }
+                           }catch (Exception e){
+                               System.out.println("Ingrese un número válido (entero positivo)");
+                           }
+                           break;
+                       }
+
+                   }
+                   default:
+                   {
+                       end=true;
+                       break;
+                   }
+               }
+           }
+       }
+
     }       
 }
     
