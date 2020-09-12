7286a6dab98b4e5797124e2df10ae9834b6b050b
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-12 17:14:18 -0500

    factorial

--- src/javaapplication2/Matematica.java
+++ src/javaapplication2/Matematica.java
@@ -14,8 +14,14 @@ public class Matematica {
 
     }
 
-    public void factorial(){
+    public void factorial(int numero){
+        double facto = 1;
+        while ( numero!=0) {
+            facto=facto*numero;
+            numero--;
+        }
 
+        System.out.println("El factorial es: " + facto);
     }
 
 }
